361ad5a73af9e2791d77b3a81c8e8818103e0f6c
jeyser <jeyser.suxe@gmail.com>
2020-08-27 17:52:49 -0500

    added validation

--- src/main/resources/db/migration/V1.0.0.0_015__INK000-000_script_alter_table_fulfillment_add_column_partial_order.sql
+++ src/main/resources/db/migration/V1.0.0.0_015__INK000-000_script_alter_table_fulfillment_add_column_partial_order.sql
@@ -2,8 +2,9 @@ ALTER TABLE `order_fulfillment`
 ADD COLUMN  `partial` TINYINT(1) NULL AFTER `notes`;
 
 ALTER TABLE `order_fulfillment_item`
-ADD COLUMN `edition_code_type` VARCHAR(12) NULL AFTER `fractional_discount`,
-ADD COLUMN `unit_price_w_discount` DECIMAL(10,2);
+ADD COLUMN  `fractional_discount` DECIMAL(10,2) NULL AFTER `fractionated`,
+ADD COLUMN  `unit_price_w_discount` DECIMAL(10,2) NULL AFTER `fractional_discount`;
+
 
 
 
