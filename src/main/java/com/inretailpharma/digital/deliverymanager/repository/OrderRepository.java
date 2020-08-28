d54e5ccaffcb296ee29197ee417845aee05a7b71
jeyser <jeyser.suxe@gmail.com>
2020-08-28 13:21:53 -0500

    added changes

--- src/main/java/com/inretailpharma/digital/deliverymanager/repository/OrderRepository.java
+++ src/main/java/com/inretailpharma/digital/deliverymanager/repository/OrderRepository.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.Set;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
