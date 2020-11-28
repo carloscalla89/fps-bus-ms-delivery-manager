78539d9b6d3b7c68f5966ab0e8de7fa99867c61d
carloscalla89 <carlos.calla89@gmail.com>
2020-11-27 21:31:12 -0500

    delete the tag data in ItemDto

--- src/main/java/com/inretailpharma/digital/deliverymanager/dto/ecommerce/ItemDto.java
+++ src/main/java/com/inretailpharma/digital/deliverymanager/dto/ecommerce/ItemDto.java
@@ -10,7 +10,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
-@Data
+@Getter
+@Setter
 public class ItemDto {
 
     @NotBlank
