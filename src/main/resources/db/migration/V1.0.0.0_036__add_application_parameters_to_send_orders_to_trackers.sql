875e222ca3c23c2665281f85046f8f99b610a69a
luisriveradev <luis.rivera@inkafarmadigital.pe>
2020-12-17 08:56:54 -0500

    updated routing logic

---
+++ src/main/resources/db/migration/V1.0.0.0_036__add_application_parameters_to_send_orders_to_trackers.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO application_parameter (code, description, value) values ('ACTIVATED_SEND_APP_IKF', 'Flag para activar envío a trackers de ordenes Inkafarma creadas por APP', 1);
+INSERT INTO application_parameter (code, description, value) values ('ACTIVATED_SEND_APP_MF', 'Flag para activar envío a trackers de ordenes Mifarma creadas por APP', 0);
+INSERT INTO application_parameter (code, description, value) values ('ACTIVATED_SEND_CALL_IKF', 'Flag para activar envío a trackers de ordenes Inkafarma creadas por Call', 1);
+INSERT INTO application_parameter (code, description, value) values ('ACTIVATED_SEND_CALL_MF', 'Flag para activar envío a trackers de ordenes Mifarma creadas por Call', 1);
+INSERT INTO application_parameter (code, description, value) values ('ACTIVATED_SEND_SC_IKF', 'Flag para activar envío a trackers de ordenes Inkafarma creadas por Seller Center', 1);
+INSERT INTO application_parameter (code, description, value) values ('ACTIVATED_SEND_SC_MF', 'Flag para activar envío a trackers de ordenes Mifarma creadas por Seller Center', 0);
+INSERT INTO application_parameter (code, description, value) values ('ACTIVATED_SEND_WEB_IKF', 'Flag para activar envío a trackers de ordenes Inkafarma creadas por Web', 1);
+INSERT INTO application_parameter (code, description, value) values ('ACTIVATED_SEND_WEB_MF', 'Flag para activar envío a trackers de ordenes Mifarma creadas por Web', 0);
