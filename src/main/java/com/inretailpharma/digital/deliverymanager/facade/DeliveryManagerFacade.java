02ae78c8d28c884d3182df7c9ca1a4eb6e0d1cd7
unknown <mtoledo@farmaciasperuanas.pe>
2021-02-03 16:22:20 -0500

    bbr-fix

--- src/main/java/com/inretailpharma/digital/deliverymanager/facade/DeliveryManagerFacade.java
+++ src/main/java/com/inretailpharma/digital/deliverymanager/facade/DeliveryManagerFacade.java
@@ -582,7 +582,6 @@ public class DeliveryManagerFacade {
                .creditCardId(orderResponseFulfillment.getCreditCardId())
                .paymentMethodId(orderResponseFulfillment.getPaymentMethodId())
                .confirmedOrder(orderResponseFulfillment.getConfirmedOrder())
-               .transactionId(orderResponseFulfillment.getTransactionId())
                     .build();
 			log.info("END FACADE getOrderByOrderNumber:"+orderNumber);
 			return Mono.just(orderResponseCanonical);
