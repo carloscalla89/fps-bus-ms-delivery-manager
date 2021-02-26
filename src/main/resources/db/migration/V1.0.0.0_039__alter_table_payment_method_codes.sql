22b6364cc55a870bd7ef7c96878eea6a81dccf86
carloscalla89 <carlos.calla89@gmail.com>
2021-02-25 19:40:17 -0500

    update the correct table for the column transaction_date_visanet

--- src/main/resources/db/migration/V1.0.0.0_039__alter_table_payment_method_codes.sql
+++ src/main/resources/db/migration/V1.0.0.0_039__alter_table_payment_method_codes.sql
@@ -4,7 +4,7 @@ ADD COLUMN `provider_card_commercial_code` VARCHAR(64) NULL DEFAULT NULL;
 ALTER TABLE `payment_method`
 ADD COLUMN `num_pan_visanet` VARCHAR(64) NULL DEFAULT NULL;
 
-ALTER TABLE `order_process_status`
+ALTER TABLE `payment_method`
 ADD COLUMN `transaction_date_visanet` DATETIME NULL DEFAULT NULL;
 
 ALTER TABLE `service_type`
