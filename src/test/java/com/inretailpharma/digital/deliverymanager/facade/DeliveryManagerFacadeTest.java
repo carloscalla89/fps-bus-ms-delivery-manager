bb5d99a99e634b69381a10f06ab9e3f5b2f6c625
carloscalla89 <carlos.calla89@gmail.com>
2021-07-12 09:15:01 -0500

    add refactoring to cath erros when the transactions update order were assigned

--- src/test/java/com/inretailpharma/digital/deliverymanager/facade/DeliveryManagerFacadeTest.java
+++ src/test/java/com/inretailpharma/digital/deliverymanager/facade/DeliveryManagerFacadeTest.java
@@ -1,15 +1,5 @@
 package com.inretailpharma.digital.deliverymanager.facade;
 
-import com.inretailpharma.digital.deliverymanager.adapter.IAuditAdapter;
-import com.inretailpharma.digital.deliverymanager.adapter.IStoreAdapter;
-import com.inretailpharma.digital.deliverymanager.adapter.ITrackerAdapter;
-import com.inretailpharma.digital.deliverymanager.canonical.fulfillmentcenter.StoreCenterCanonical;
-import com.inretailpharma.digital.deliverymanager.canonical.manager.OrderCanonical;
-import com.inretailpharma.digital.deliverymanager.dto.OrderDto;
-import com.inretailpharma.digital.deliverymanager.entity.OrderFulfillment;
-import com.inretailpharma.digital.deliverymanager.mapper.ObjectToMapper;
-import com.inretailpharma.digital.deliverymanager.service.ApplicationParameterService;
-import com.inretailpharma.digital.deliverymanager.transactions.OrderTransaction;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,54 +7,39 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DeliveryManagerFacadeTest {
 
-    @Autowired
-    private DeliveryManagerFacade deliveryManagerFacade;
-
-    @MockBean
-    private OrderTransaction orderTransaction;
-
-    @MockBean
-    private ObjectToMapper objectToMapper;
-
-    @MockBean
-    private ApplicationParameterService applicationParameterService;
-
-    @MockBean
-    private IStoreAdapter iStoreAdapter;
-
-    @MockBean
-    private IAuditAdapter iAuditAdapter;
-
-    @MockBean
-    private LiquidationFacade liquidationFacade;
 
     @Test
-    void createOrder() {
+    public void testOnErrorContinue() throws InterruptedException {
 
-        OrderDto orderDto = new OrderDto();
-        orderDto.setEcommercePurchaseId(123456L);
-        orderDto.setLocalCode("FJ7");
+        Flux<Integer> fluxFromJust =
+                Flux.just(1, 2,3,4,5)
+                        .flatMap(val -> {
+                            if (val == 3) {
+                                return Flux.error(new RuntimeException("Test"));
+                            }
+                            return Flux.just(val*2);
+                        })
+                        .onErrorContinue((e,i)->{
+                            System.out.println("Error For Item +" + i );
+                        }).doOnNext(val -> System.out.println("final Item +" + val ))
 
-        when(iStoreAdapter.getStoreByCompanyCodeAndLocalCode(anyString(), anyString())).thenReturn(Mono.just(new StoreCenterCanonical()));
 
-        when(objectToMapper.convertOrderdtoToOrderEntity(orderDto)).thenReturn(new OrderFulfillment());
 
-        when(orderTransaction.processOrderTransaction(any(OrderFulfillment.class), any(OrderDto.class), any(StoreCenterCanonical.class))).thenReturn(new OrderCanonical());
-
-        when(iAuditAdapter.createAudit(any(OrderCanonical.class), anyString())).thenReturn(Mono.just(new OrderCanonical()));
-    }
-
-    @Test
-    void getUpdateOrder() {
+                ;
+        StepVerifier
+                .create(fluxFromJust)
+                .expectNext(2,4, 8,10)
+                .verifyComplete();
     }
 }
