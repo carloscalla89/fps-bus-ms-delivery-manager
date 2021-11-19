d46ac9b82375e493ff0997ab02aee7905889ddb3
jeyser <jeyser.suxe@inkafarmadigital.pe>
2021-11-19 10:12:49 -0500

    refactor querys

--- src/main/java/com/inretailpharma/digital/deliverymanager/rest/OrderInfoRest.java
+++ src/main/java/com/inretailpharma/digital/deliverymanager/rest/OrderInfoRest.java
@@ -14,34 +14,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @RestController
-@RequestMapping(value = "/fulfillment")
+@RequestMapping(value = "/order")
 @Slf4j
 @AllArgsConstructor
 public class OrderInfoRest {
 
   private DeliveryManagerFacade deliveryManagerFacade;
 
-  @PostMapping(value = "/orderinfo", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/info", produces = MediaType.APPLICATION_JSON_VALUE)
   public OrderCanonicalResponse getOrder(@RequestBody RequestFilterDTO filter) {
     log.info("[START] endpoint /fulfillment/order {}");
     return deliveryManagerFacade.getOrder(filter);
   }
 
-  @GetMapping(value = "/order/status", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "/status", produces = MediaType.APPLICATION_JSON_VALUE)
   public Flux<OrderStatusDto> getOrderStatus() {
     log.info("[START] endpoint /fulfillment/orderStatus {}");
     return deliveryManagerFacade.getAllOrderStatus().subscribeOn(Schedulers.parallel());
 
   }
 
-  @GetMapping(value = "/order/detail/{ecommerceId}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "/detail/{ecommerceId}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Mono<OrderInfoConsolidated> getOrderInfoClient(@PathVariable(value = "ecommerceId") long ecommerceId) {
     log.info("[START] endpoint /order/detail/client");
     return deliveryManagerFacade.getOrderInfoDetail(ecommerceId);
