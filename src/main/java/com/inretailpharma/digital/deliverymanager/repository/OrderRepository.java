a38630e50c7ce889bcebffd6599e9c3f3ce198d1
Ruben <ceval1004@gmail.com>
2021-12-03 15:52:10 -0500

    fix query

--- src/main/java/com/inretailpharma/digital/deliverymanager/repository/OrderRepository.java
+++ src/main/java/com/inretailpharma/digital/deliverymanager/repository/OrderRepository.java
@@ -72,7 +72,7 @@ public interface OrderRepository extends JpaRepository<OrderFulfillment, Long> {
             "af.department, af.notes, af.latitude, af.longitude, o.partial, " +
             "o.subTotalWithNoSpecificPaymentMethod, o.totalWithNoSpecificPaymentMethod, o.totalWithPaymentMethod, " + // referentes a 3 precios
             "o.paymentMethodCardType, o.discountAppliedNoDP, " + // referentes a 3 precios
-            "os.liquidationEnabled, os.liquidationStatus, o.mixedOrder, o.group_id " +
+            "os.liquidationEnabled, os.liquidationStatus, o.mixedOrder, o.group_id as groupId " +
             "from order_fulfillment o " +
             "inner join client_fulfillment c on c.id = o.client_id " +
             "inner join order_process_status s on o.id = s.order_fulfillment_id " +
