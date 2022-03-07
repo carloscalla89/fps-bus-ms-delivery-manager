3aa277a3e6ecf0cabd62d7cb70b0971725c6e5c4
Paul Victor Inche Pena <pinche@farmaciasperuanas.pe>
2022-03-07 15:34:26 -0500

    fix included errors in order process

--- src/main/java/com/inretailpharma/digital/deliverymanager/util/sql/CustomSqlQuery.java
+++ src/main/java/com/inretailpharma/digital/deliverymanager/util/sql/CustomSqlQuery.java
@@ -3,14 +3,15 @@ package com.inretailpharma.digital.deliverymanager.util.sql;
 public class CustomSqlQuery {
 
   public static final StringBuilder BASIC_QUERY_GET_ORDERINFO = new StringBuilder()
-      .append("select o.id as orderId, o.ecommerce_purchase_id as ecommerceId, o.source,  "
-              + "o.scheduled_time as scheduledTime, os.type as statusName,  s.center_code as centerCode,  "
-              + "s.company_code as companyCode, st.source_channel as serviceChannel, st.short_code as serviceTypeShortCode,  "
-              + "c.first_name as firstName,  c.document_number as documentNumber,  c.last_name as lastName, os.code as statusCode "
-              + "from order_fulfillment o  "
+      .append("select o.id as orderId, o.ecommerce_purchase_id as ecommerceId, o.source, "
+              + "o.scheduled_time as scheduledTime, os.type as statusName, s.center_code as centerCode, "
+              + "s.company_code as companyCode, st.source_channel as serviceChannel, "
+              + "st.short_code as serviceTypeShortCode, c.first_name as firstName, c.document_number as documentNumber, "
+              + "c.last_name as lastName, os.code as statusCode "
+              + "from order_fulfillment o "
               + "inner join client_fulfillment c on c.id = o.client_id "
-              + "inner join order_process_status s on o.id = s.order_fulfillment_id  "
-              + "inner join order_status os on os.code = s.order_status_code  "
+              + "inner join order_process_status s on o.id = s.order_fulfillment_id "
+              + "inner join order_status os on os.code = s.order_status_code "
               + "inner join service_type st on st.code = s.service_type_code ");
 
   public static final StringBuilder BASIC_QUERY_ORDER_BY_DATE_AND_ORDER_NUMBER_DESC = new StringBuilder()
