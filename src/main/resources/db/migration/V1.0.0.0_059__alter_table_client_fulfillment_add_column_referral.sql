cff504f6261a3f16c716bb76e5c649f4be46d88b
DEValdivieso <developerbackend3@farmaciasperuanas.pe>
2022-07-05 16:19:48 -0500

    delete version 59 referral

--- src/main/resources/db/migration/V1.0.0.0_059__alter_table_client_fulfillment_add_column_referral.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `client_fulfillment`
-    ADD COLUMN `referral_code` VARCHAR(20) NULL DEFAULT NULL;
-
-ALTER TABLE `client_fulfillment`
-    ADD COLUMN `referral_msg` VARCHAR(300) NULL DEFAULT NULL;
