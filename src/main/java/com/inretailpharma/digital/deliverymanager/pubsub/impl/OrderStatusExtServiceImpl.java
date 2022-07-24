11d96600a780e9b493b4a22ad22b42bc7b26f4a2
luisriveradev <luis.rivera@inkafarmadigital.pe>
2022-07-24 14:03:35 -0500

    make pubsub conditional by profile

--- src/main/java/com/inretailpharma/digital/deliverymanager/pubsub/impl/OrderStatusExtServiceImpl.java
+++ src/main/java/com/inretailpharma/digital/deliverymanager/pubsub/impl/OrderStatusExtServiceImpl.java
@@ -6,10 +6,12 @@ import com.inretailpharma.digital.deliverymanager.pubsub.OrderStatusService;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 
 @Slf4j
+@ConditionalOnProperty(value="spring.cloud.gcp.project-id")
 @EnableBinding(OrderStatusService.class)
 public class OrderStatusExtServiceImpl {
 
